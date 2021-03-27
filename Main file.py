0bcb5faf5688db860898c23934fa018342092803
DionisPalpatin <77577723+DionisPalpatin@users.noreply.github.com>
2021-03-27 11:51:12 +0300

    Add files via upload

---
+++ Main file.py
@@ -0,0 +1,22 @@
+import pygame as pg
+from Const import figure
+
+
+pg.init()
+figure.screen.fill((255, 255, 255))
+
+
+figure.drowing_field()
+pg.display.update()
+
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            exit()
+
+
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            figure.game_process()
+            pg.display.update()
